32928b09e380349775d5f4ddfa0b7a99b1ab6dc2
Nicolas Belouin <kynarth.alseif@gmail.com>
2015-11-18 22:57:14 +0100

    Fix test of the qrc command.

--- tests/test_qrc.py
+++ tests/test_qrc.py
@@ -2,6 +2,7 @@ import os
 import re
 import pytest
 
+from lxml import etree
 from click.testing import CliRunner
 
 from pyqtcli.cli import pyqtcli
@@ -60,12 +61,15 @@ def test_duplication_of_qresource():
         )
 
 
-def test_qrc_command(qrc_contents):
+def test_qrc_command():
     runner = CliRunner()
     # Location of resource folder for testing
     abs_res_folder = os.path.abspath("tests/resources")
 
     with runner.isolated_filesystem():
+        # Relative path to resource folder from test folder
+        rel_res_folder = os.path.relpath(abs_res_folder)
+
         # launch makerc command
         result = runner.invoke(
             pyqtcli, ["qrc", "-v", abs_res_folder, "-o", "test"]
@@ -75,7 +79,45 @@ def test_qrc_command(qrc_contents):
         print("verbose:\n" + result.output)
         assert os.path.isfile("test.qrc")
 
-        # Check qrc contents
-        with open("test.qrc", "r") as qrc:
-            for x, y in zip(qrc, qrc_contents.split("\n")):
-                assert x.strip() == y.strip()
+        # Parse newly generated qrc file
+        tree = etree.parse("test.qrc")
+        root = tree.getroot()
+
+        # Check qresources and their prefix attribute corresponding to
+        # directories of the first level of resources folder
+        first_dirs = [
+            "/" + d for d in os.listdir(abs_res_folder)
+            if os.path.isdir(os.path.join(abs_res_folder, d))
+        ]
+        first_dirs.append("/")
+
+        for qresource in root.iter(tag="qresource"):
+            assert qresource.attrib.get("prefix") in first_dirs
+            directory = first_dirs.pop(
+                first_dirs.index(qresource.attrib.get("prefix"))
+            )
+
+            # Get all resources files contained in the current qresource
+            res = [resource.text for resource in qresource.iter(tag="file")]
+
+            # Special case for root directory to not get into sub directories
+            if directory == "/":
+                resources = [
+                    os.path.join(rel_res_folder, r)
+                    for r in os.listdir(abs_res_folder)
+                    if os.path.isfile(os.path.join(abs_res_folder, r))
+                ]
+
+                for resource in resources:
+                    assert resource in res
+
+                continue
+
+            # Search if resource files have been recorded correctly in other
+            # directories
+            for root, dirs, files in os.walk(rel_res_folder + directory):
+                for file_path in files:
+                    assert os.path.join(root, file_path) in res
+
+        # Verify if all dirs have been checked
+        assert first_dirs == []
