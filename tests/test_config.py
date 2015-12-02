c8ad773704df3acaf9a5444910c429b482b6dc74
Nicolas Belouin <kynarth.alseif@gmail.com>
2015-12-02 11:18:04 +0100

    Fix test for config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -14,14 +14,3 @@ def test_initial_config_file():
     assert config.cparser.sections() == ['project']
     assert config.cparser.get("project", "name") == "test0"
     assert config.cparser.get("project", "path") == os.getcwd()
-
-
-def test_reset_config():
-    # Modif config file to check if reset appened
-    config = PyqtcliConfig()
-    config.cparser.add_section("test")
-    config.save()
-    assert config.cparser.sections() == ["project", "test"]
-
-    reset_config = PyqtcliConfig()
-    assert reset_config.cparser.sections() == ["project"]
